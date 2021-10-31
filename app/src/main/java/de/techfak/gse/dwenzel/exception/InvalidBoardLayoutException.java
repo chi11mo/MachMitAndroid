133472725a973b7a41b6cb6b2fe758eaee5478f3
Dominic Wenzel <dwenzel@techfak.uni-bielefeld.de>
2021-10-31 16:24:44 +0100

    refs  #32940 create board and field exceptions @0.2

---
+++ app/src/main/java/de/techfak/gse/dwenzel/exception/InvalidBoardLayoutException.java
@@ -0,0 +1,30 @@
+/**
+ * Exception for invalid board layout.
+ */
+package de.techfak.gse.dwenzel.exception;
+
+
+
+/**
+ * The type Invalid board layout exception.
+ */
+public class InvalidBoardLayoutException extends Exception {
+
+    private static final long serialVersionUID = 378322;
+
+    /**
+     * Instantiates a new Invalid board layout exception.
+     */
+    public InvalidBoardLayoutException() {
+        super("InvalidBoardLayout");
+    }
+
+    /**
+     * Instantiates a new Invalid board layout exception.
+     *
+     * @param exception the exception
+     */
+    public InvalidBoardLayoutException(final String exception) {
+        super(exception);
+    }
+}
---
+++ app/src/main/java/de/techfak/gse/dwenzel/exception/InvalidFieldException.java
@@ -0,0 +1,31 @@
+/**
+ * Exception for Invalid Fields.
+ */
+package de.techfak.gse.dwenzel.exception;
+
+
+
+/**
+ * The type Invalid field exception.
+ */
+public class InvalidFieldException extends Exception {
+
+    private static final long serialVersionUID = 378324;
+
+
+    /**
+     * Instantiates a new Invalid field exception.
+     */
+    public InvalidFieldException() {
+        super("InvalidField");
+    }
+
+    /**
+     * Instantiates a new Invalid field exception.
+     *
+     * @param exception the exception
+     */
+    public InvalidFieldException(final String exception) {
+        super(exception);
+    }
+}
