ceb1d4b055c405fd8d5372e2eaaaf4bede075a00
Dominic Wenzel <dwenzel@techfak.de>
2021-12-05 10:35:55 +0100

    closes #37800 @3 fix small bug and false commit

--- app/src/main/java/de/techfak/gse/dwenzel/game_screen/map/GreenAbstractField.java
+++ app/src/main/java/de/techfak/gse/dwenzel/game_screen/map/GreenAbstractField.java
@@ -6,7 +6,7 @@ import de.techfak.gse.dwenzel.game_screen.model.ButtonSpriteSheet;
 
 public class GreenAbstractField extends AbstractField {
     private final Drawable drawableField;
-    private ButtonSpriteSheet buttonSpriteSheet;l
+    private ButtonSpriteSheet buttonSpriteSheet;
     /**
      * Method to create field.
      * @param isCrossed test is field cross.
